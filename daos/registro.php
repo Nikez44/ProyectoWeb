52ef0ee268743c6fc0199297ad44755004167829
Nikez44 <nike-o_94@hotmail.com>
2015-05-13 18:24:03 -0500

    ya registra usuarios

--- daos/registro.php
+++ daos/registro.php
@@ -9,29 +9,28 @@
 
 include 'global.php';
 
+$nombre = $_POST['Nombre'];
+$apellido = $_POST['Apellidos'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password2 = $_POST['password2'];
 
-if ($_POST['Nombre'] == '' or $_POST['Apellidos'] == '' or $_POST['email'] == '' or $_POST['password'] == '') {
+if ($nombre == '' or $apellido == '' or $email == '' or $password == '') {
     echo 'Por favor llene todos los campos.';
 } else {
-    $sql = "SELECT * FROM usuarios WHERE email = '$_POST[email]'";
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
     $result = ejecutar_query($sql);
 
+    if(!$result->num_rows > 0){
+        if($password == $password2){
 
-    if($result->num_rows > 0){
-        if($_POST['password'] == $_POST['password2'])
-        {
-            $nombre = $_POST['Nombre'];
-            $apellido = $_POST['Apellidos'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
             $sql = "INSERT INTO `proyectoweb`.`usuarios` (`name`, `apellido`, `email`, `password`, `admin`) VALUES ('$nombre', '$apellido', '$email', '$password', '0')";
             ejecutar_query($sql);
 
             echo 'Usted se ha registrado correctamente.';
 
-        }
-        else
-        {
+        } else {
             echo 'Las claves no son iguales, intente nuevamente.';
         }
     } else {
