18e8a7795fd87d2310e0a76d6b760c65d7c059fe
Kevin Pacheco <kev_in.13@hotmail.com>
2015-05-13 12:07:49 -0500

    Se  modifico el registro

--- login.php
+++ login.php
@@ -11,11 +11,11 @@ $password = $_POST["password"];
 
 session_start();
 
-$conexion = mysql_connect("localhost","root","root");
+$conexion = mysql_connect("localhost","root","08001421");
 mysql_select_db("web",$conexion);
 
 
-$sql = "SELECT usuario,password FROM usuarios WHERE usuario = '$usuario' AND password='$password'";
+$sql = "SELECT usuario_nombre,usuario_clave FROM users WHERE usuario_email = '$usuario' AND usuario_clave='$password'";
 
 $comprobar = mysql_query($sql);
 
--- registro.php
+++ registro.php
@@ -5,3 +5,44 @@
  * Date: 13/05/15
  * Time: 11:49 AM
  */
+
+
+$conexion = mysql_connect("localhost", "root", "08001421");
+mysql_select_db("web", $conexion);
+
+
+if ($_POST['Nombre'] == '' or $_POST['Apellidos'] == '' or $_POST['email'] == '' or $_POST['password'] == '') {
+    echo 'Por favor llene todos los campos.';
+} else {
+    $sql = 'SELECT * FROM users';
+    $rec = mysql_query($sql);
+    $existe_Usuario = false;
+
+    while ($result = mysql_fetch_object($rec)) {
+        if ($result->email == $_POST['email']) {
+            $existe_Usuario = true;
+        }
+    }
+
+    if (!$existe_Usuario) {
+
+        if($_POST['password'] == $_POST['password2'])
+        {
+            $nombre = $_POST['Nombre'];
+            $apellido = $_POST['Apellidos'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $sql = "INSERT INTO users (usuario_nombre,usuario_apellido,usuario_clave, usuario_email) VALUES ('$nombre','$apellido', '$password', '$email')";
+            mysql_query($sql);
+
+            echo 'Usted se ha registrado correctamente.';
+
+        }
+        else
+        {
+            echo 'Las claves no son iguales, intente nuevamente.';
+        }
+    } else {
+        echo 'Este usuario ya ha sido registrado anteriormente.';
+    }
+}
