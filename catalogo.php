5619a71be684efb265df5188ad413c0fd1745f65
Kevin Pacheco <kev_in.13@hotmail.com>
2015-05-28 13:58:09 -0500

    se creo el catalogo de los productos como una lista

---
+++ catalogo.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="styles/index.css"/>
+</head>
+<body>
+
+<?php include '_header.php' ?>
+
+    <div class="catalogo">
+
+    </div>
+
+<?php include '_footer.php' ?>
+</body>
+</html>
