c4a9e8a4a24716674f90b8412c4243d1439bfa22
Kevin Pacheco <kev_in.13@hotmail.com>
2015-05-28 13:59:02 -0500

    Se modifico el catalogo

--- catalogo.php
+++ catalogo.php
@@ -1,4 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyuchiha
+ * Date: 13/05/2015
+ * Time: 07:04 PM
+ */
 
+include 'daos/show_all_juegos.php';
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -9,12 +19,27 @@
 </head>
 <body>
 
-<?php include '_header.php' ?>
+    <?php include '_header.php' ?>
 
+    <center>
     <div class="catalogo">
 
-    </div>
+        <ul>
+            <?php foreach($juegos as $juego): ?>
+                <li>
+                    <img src=" <?php echo $juego['imagen'] ?> ">
+                    <h3><?php echo $juego['nombre'] ?></h3>
+                    <p></p>
+                    <h4><?php echo $juego['categoria_id'] ?></h4>
+                    <h5><?php echo $juego['consola_id'] ?></h5>
+                    <h4><?php echo $juego['precio'] ?></h4>
+                    <button>Comprar</button>
+                </li>
+            <?php endforeach ?>
+        </ul>
 
+    </div>
+    </center>
 <?php include '_footer.php' ?>
 </body>
 </html>
