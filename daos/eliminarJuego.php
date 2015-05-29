c365d00baf954f783a89b378103d563a5ca6e677
Nikez44 <nike-o_94@hotmail.com>
2015-05-29 18:01:27 -0500

    se elimina la imagen cuando se elimina la informacion en la base de datos

--- daos/eliminarJuego.php
+++ daos/eliminarJuego.php
@@ -6,12 +6,17 @@
  * Time: 10:30 PM
  */
 
-
+include 'global.php';
 
 $id = $_GET['id'];
 
-$query = "DELETE FROM `proyectoweb`.`juegos` WHERE `juegos`.`id` = $id";
+$query = "SELECT * FROM `juegos` WHERE `id` = $id";
+$juego = ejecutar_query($query)->fetch_array();
+//Elimino el archivo de la imagen del juego
+unlink("../".$juego['imagen']);
 
+//Elimino el juego de la base de datos
+$query = "DELETE FROM `proyectoweb`.`juegos` WHERE `juegos`.`id` = $id";
 ejecutar_query($query);
 
 header("Location: ../juegos.php");
