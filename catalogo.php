09ebf137549ebe1f0e3c24b13b5ef7a15acf55e1
Kevin Pacheco <kev_in.13@hotmail.com>
2015-05-28 21:34:18 -0500

    Se modifico el css para desplegar cuadriculado

--- catalogo.php
+++ catalogo.php
@@ -23,25 +23,26 @@ include 'daos/show_all_juegos.php';
     <?php include '_header.php' ?>
 
     <center>
+
     <div class="catalogo">
-        <ul class="catalog-list">
+        <ul class="catalog-list" id="triple">
             <?php foreach($juegos as $juego): ?>
                 <li>
                     <div class="Catalog-Item">
                         <div class="Game-Image"><img src=" <?php echo $juego['imagen'] ?> "></div>
                         <div class="Catalog-Title"><h1><?php echo $juego['nombre'] ?></h1></div>
-                        <div class="Catalog-Category"><h4><?php echo $juego['categoria_id'] ?></h4></div>
-                        <div class="Catalog-Console"><h5><?php echo $juego['consola_id'] ?></h5></div>
-                        <div class="Catalog-Price"><h4><?php echo $juego['precio'] ?></h4></div>
+                        <div class="Catalog-Category"><h2>Categoria: <?php echo $juego['categoria_id'] ?></h2></div>
+                        <div class="Catalog-Console"><h3>Disponible para: <?php echo $juego['consola_id'] ?></h3></div>
+                        <div class="Catalog-Price"><h3>Precio: <?php echo $juego['precio'] ?></h3></div>
 
                         <button onclick="location.href = 'Juego.html';">Comprar</button>
                     </div>
                 </li>
             <?php endforeach ?>
         </ul>
-
     </div>
     </center>
+
 <?php include '_footer.php' ?>
 </body>
 </html>
