e7f4d6307fff395b254db7646c4468c5d83dcea7
Nikez44 <nike-o_94@hotmail.com>
2015-05-30 16:08:59 -0500

    corregidas las querys para eliminar solo el carrito del cliente

--- daos/comprar.php
+++ daos/comprar.php
@@ -5,14 +5,17 @@
  * Date: 30/05/2015
  * Time: 01:32 PM
  */
+session_start();
 
 require 'global.php';
 
-$query = "INSERT INTO ventas (usuarios_id, juegos_id, cantidad, fecha) SELECT usuarios_id, juegos_id, cantidad, fecha FROM usuarios_has_juegos";
+$id = $_SESSION['u_id'];
+$query = "INSERT INTO ventas (usuarios_id, juegos_id, cantidad, fecha) SELECT usuarios_id, juegos_id, cantidad, fecha FROM usuarios_has_juegos WHERE usuarios_id = $id";
 
 ejecutar_query($query);
 
-$query = "TRUNCATE TABLE  usuarios_has_juegos";
+$query = "DELETE FROM usuarios_has_juegos WHERE usuarios_id = $id";
 
 ejecutar_query($query);
 
+header("Location: ../carrito.php");
