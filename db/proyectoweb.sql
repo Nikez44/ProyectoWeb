484c045a1c24e347a2c8b37d47e6de3a5671dc3b
Nikez44 <nike-o_94@hotmail.com>
2015-05-31 14:16:31 -0500

    sql actualizado

--- db/proyectoweb.sql
+++ db/proyectoweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2015 a las 22:19:14
+-- Tiempo de generación: 31-05-2015 a las 21:15:32
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -63,7 +63,8 @@ INSERT INTO `consola` (`id`, `nombre`) VALUES
 (3, 'XBOX 360'),
 (4, 'XBOX ONE'),
 (5, 'WII'),
-(6, 'WII U');
+(6, 'WII U'),
+(7, 'PC');
 
 -- --------------------------------------------------------
 
@@ -79,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `juegos` (
   `imagen` varchar(45) CHARACTER SET utf8 NOT NULL,
   `categoria_id` int(11) NOT NULL DEFAULT '0',
   `consola_id` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `juegos`
@@ -89,9 +90,17 @@ INSERT INTO `juegos` (`id`, `nombre`, `precio`, `cantidad`, `imagen`, `categoria
 (1, 'Gears Of War 3', 399.99, 20, 'images/games/568979338.jpg', 5, 3),
 (2, 'Mortal Kombat X', 999.99, 50, 'images/games/654164761.jpg', 1, 2),
 (3, 'FIFA 15', 899.99, 60, 'images/games/572742052.jpg', 3, 4),
-(5, 'Super Smash Bros Brawl', 599.9, 90, 'images/games/127483241.jpg', 1, 5),
-(6, 'Mario Kart 8', 999.99, 90, 'images/games/446941233.jpg', 2, 6),
-(7, 'Halo 4', 499.9, 30, 'images/games/578992711.jpg', 5, 3);
+(5, 'Brawl', 599.9, 90, 'images/games/127483241.jpg', 1, 5),
+(6, 'Mario Kart 8', 39.99, 90, 'images/games/446941233.jpg', 2, 6),
+(7, 'Halo 4', 499.9, 30, 'images/games/578992711.jpg', 5, 3),
+(8, 'NFS U. 3', 799.99, 20, 'images/games/851947505.png', 2, 4),
+(9, 'NFS Rivals ', 550.5, 20, 'images/games/629449609.jpg', 2, 1),
+(10, 'Avengers', 399, 10, 'images/games/760225543.jpg', 4, 3),
+(11, 'Halo 3', 350.9, 30, 'images/games/21339993.jpg', 5, 3),
+(12, 'FIFA 15', 999.99, 50, 'images/games/440913939.jpg', 3, 2),
+(13, 'GTA V', 500, 25, 'images/games/49482373.png', 4, 7),
+(14, 'Wolfenstein', 750.5, 5, 'images/games/351912413.jpg', 4, 2),
+(15, 'TLOZ Skyward', 600, 30, 'images/games/360177492.jpg', 4, 5);
 
 -- --------------------------------------------------------
 
@@ -113,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `name`, `apellido`, `email`, `password`, `admin`) VALUES
-(1, 'admin', 'admin', 'admin@hotmail.com', 'adminadmin', 1),
+(1, 'admin', 'Adminmin', 'admin@hotmail.com', 'adminadmin', 1),
 (2, 'cliente', 'cliente', 'cliente@hotmail.com', 'cliente123', 0),
 (3, 'Oscar', 'Perez', 'nike-o_94@hotmail.com', 'holahola', 0);
 
@@ -130,6 +139,15 @@ CREATE TABLE IF NOT EXISTS `usuarios_has_juegos` (
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `usuarios_has_juegos`
+--
+
+INSERT INTO `usuarios_has_juegos` (`usuarios_id`, `juegos_id`, `cantidad`, `fecha`) VALUES
+(2, 1, 1, '2015-05-31'),
+(2, 9, 1, '2015-05-31'),
+(3, 3, 1, '2015-05-30');
+
 -- --------------------------------------------------------
 
 --
@@ -142,7 +160,7 @@ CREATE TABLE IF NOT EXISTS `ventas` (
   `juegos_id` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `fecha` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `ventas`
@@ -155,7 +173,18 @@ INSERT INTO `ventas` (`ventas_id`, `usuarios_id`, `juegos_id`, `cantidad`, `fech
 (4, 3, 1, 8, '2015-05-29'),
 (5, 3, 3, 4, '2015-05-30'),
 (6, 3, 6, 7, '2015-05-30'),
-(8, 3, 6, 5, '2015-04-15');
+(8, 3, 6, 5, '2015-04-15'),
+(9, 2, 3, 1, '2015-05-30'),
+(10, 2, 5, 1, '2015-05-30'),
+(11, 2, 6, 1, '2015-05-30'),
+(12, 3, 2, 3, '2015-05-30'),
+(13, 3, 7, 2, '2015-05-30'),
+(15, 3, 1, 4, '2015-05-30'),
+(16, 2, 2, 1, '2015-05-30'),
+(17, 2, 3, 1, '2015-05-31'),
+(18, 2, 5, 1, '2015-05-30'),
+(19, 2, 6, 1, '2015-05-30'),
+(20, 2, 7, 1, '2015-05-30');
 
 --
 -- Índices para tablas volcadas
@@ -205,7 +234,7 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `juegos`
 --
 ALTER TABLE `juegos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
@@ -215,7 +244,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-MODIFY `ventas_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `ventas_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 --
 -- Restricciones para tablas volcadas
 --
